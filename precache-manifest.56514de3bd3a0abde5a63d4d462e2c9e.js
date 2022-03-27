ddbfedc4adbb182c8fcf4c60440e1830a2f7edaf
Nara <736264660@qq.com>
2022-03-28 05:56:09 +0800

    first commit

---
+++ precache-manifest.56514de3bd3a0abde5a63d4d462e2c9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1c147eaba4d3d522da",
+    "url": "/Daily-Billings-Website/css/app.366d67aa.css"
+  },
+  {
+    "revision": "b96a2e70d10db01f6c7a",
+    "url": "/Daily-Billings-Website/css/chunk-vendors.71a233fb.css"
+  },
+  {
+    "revision": "fb3e576efcc417278701087d57cc7c42",
+    "url": "/Daily-Billings-Website/index.html"
+  },
+  {
+    "revision": "3e1c147eaba4d3d522da",
+    "url": "/Daily-Billings-Website/js/app.0d211133.js"
+  },
+  {
+    "revision": "b96a2e70d10db01f6c7a",
+    "url": "/Daily-Billings-Website/js/chunk-vendors.0b322b6c.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/Daily-Billings-Website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Daily-Billings-Website/robots.txt"
+  }
+]);
